f9cddf4c82dcf2e094ba21ddc51ceaa4513668f6
Dino Maric <dino.onex@gmail.com>
2023-07-02 19:01:23 +0200

    Add `use_bundler` option
    `user_bundler` if set to `true` will run `erb_lint` wiht `bundel exec`.
    Defaults to `false`

--- script.sh
+++ script.sh
@@ -1,10 +1,9 @@
 #!/bin/sh -e
 
-if [ -n "${GITHUB_WORKSPACE}" ]
-then
-    git config --global --add safe.directory "${GITHUB_WORKSPACE}" || exit 1
-    git config --global --add safe.directory "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit 1
-    cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit 1
+if [ -n "${GITHUB_WORKSPACE}" ]; then
+	git config --global --add safe.directory "${GITHUB_WORKSPACE}" || exit 1
+	git config --global --add safe.directory "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit 1
+	cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit 1
 fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
@@ -19,34 +18,40 @@ echo '::endgroup::'
 echo '::group:: Installing erb-lint with extensions ... https://github.com/Shopify/erb-lint'
 # if 'gemfile' erb-lint version selected
 if [ "$INPUT_ERBLINT_VERSION" = "gemfile" ]; then
-  # if Gemfile.lock is here
-  if [ -f 'Gemfile.lock' ]; then
-    # grep for erb-lint version
-    ERBLINT_GEMFILE_VERSION=$(ruby -ne 'print $& if /^\s{4}erb_lint\s\(\K.*(?=\))/' Gemfile.lock)
+	# if Gemfile.lock is here
+	if [ -f 'Gemfile.lock' ]; then
+		# grep for erb-lint version
+		ERBLINT_GEMFILE_VERSION=$(ruby -ne 'print $& if /^\s{4}erb_lint\s\(\K.*(?=\))/' Gemfile.lock)
 
-    # if erb-lint version found, then pass it to the gem install
-    # left it empty otherwise, so no version will be passed
-    if [ -n "$ERBLINT_GEMFILE_VERSION" ]; then
-      ERBLINT_VERSION=$ERBLINT_GEMFILE_VERSION
-      else
-        printf "Cannot get the erb-lint's version from Gemfile.lock. The latest version will be installed."
-    fi
-    else
-      printf 'Gemfile.lock not found. The latest version will be installed.'
-  fi
-  else
-    # set desired erb-lint version
-    ERBLINT_VERSION=$INPUT_ERBLINT_VERSION
+		# if erb-lint version found, then pass it to the gem install
+		# left it empty otherwise, so no version will be passed
+		if [ -n "$ERBLINT_GEMFILE_VERSION" ]; then
+			ERBLINT_VERSION=$ERBLINT_GEMFILE_VERSION
+		else
+			printf "Cannot get the erb-lint's version from Gemfile.lock. The latest version will be installed."
+		fi
+	else
+		printf 'Gemfile.lock not found. The latest version will be installed.'
+	fi
+else
+	# set desired erb-lint version
+	ERBLINT_VERSION=$INPUT_ERBLINT_VERSION
 fi
 
 gem install -N erb_lint --version "${ERBLINT_VERSION}"
 echo '::endgroup::'
 
+if [ "${INPUT_USE_BUNDLER}" = "false" ]; then
+	BUNDLE_EXEC=""
+else
+	BUNDLE_EXEC="bundle exec "
+fi
+
 echo '::group:: Running erb-lint with reviewdog 🐶 ...'
-erblint --lint-all --format compact \
-  | reviewdog \
-      -efm="%f:%l:%c: %m" \
-      -reporter="${INPUT_REPORTER}" \
-      -filter-mode="${INPUT_FILTER_MODE}" \
-      -level="${INPUT_LEVEL}"
+${BUNDLE_EXEC}erblint --lint-all --format compact |
+	reviewdog \
+		-efm="%f:%l:%c: %m" \
+		-reporter="${INPUT_REPORTER}" \
+		-filter-mode="${INPUT_FILTER_MODE}" \
+		-level="${INPUT_LEVEL}"
 echo '::endgroup::'
