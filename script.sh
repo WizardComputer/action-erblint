d32e05d25145926f87b5231fa9fa5cc6906e61e8
Dino Maric <dino.onex@gmail.com>
2023-07-03 11:40:18 +0200

    Refactor script

--- script.sh
+++ script.sh
@@ -1,57 +1,69 @@
 #!/bin/sh -e
 
-if [ -n "${GITHUB_WORKSPACE}" ]; then
-	git config --global --add safe.directory "${GITHUB_WORKSPACE}" || exit 1
-	git config --global --add safe.directory "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit 1
-	cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit 1
-fi
+setup_git() {
+	if [ -n "${GITHUB_WORKSPACE}" ]; then
+		git config --global --add safe.directory "${GITHUB_WORKSPACE}" || exit 1
+		git config --global --add safe.directory "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit 1
+		cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit 1
+	fi
+}
 
-export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
+setup_reviewdog() {
+	export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
+	TEMP_PATH="$(mktemp -d)"
+	PATH="${TEMP_PATH}:$PATH"
+	echo '::group::🐶 Installing reviewdog ... https://github.com/reviewdog/reviewdog'
+	curl -sfL https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh | sh -s -- -b "${TEMP_PATH}" "${REVIEWDOG_VERSION}" 2>&1
+	echo '::endgroup::'
+}
 
-TEMP_PATH="$(mktemp -d)"
-PATH="${TEMP_PATH}:$PATH"
-
-echo '::group::🐶 Installing reviewdog ... https://github.com/reviewdog/reviewdog'
-curl -sfL https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh | sh -s -- -b "${TEMP_PATH}" "${REVIEWDOG_VERSION}" 2>&1
-echo '::endgroup::'
-
-echo '::group:: Installing erb-lint with extensions ... https://github.com/Shopify/erb-lint'
-# if 'gemfile' erb-lint version selected
-if [ "$INPUT_ERBLINT_VERSION" = "gemfile" ]; then
-	# if Gemfile.lock is here
-	if [ -f 'Gemfile.lock' ]; then
-		# grep for erb-lint version
-		ERBLINT_GEMFILE_VERSION=$(ruby -ne 'print $& if /^\s{4}erb_lint\s\(\K.*(?=\))/' Gemfile.lock)
-
-		# if erb-lint version found, then pass it to the gem install
-		# left it empty otherwise, so no version will be passed
-		if [ -n "$ERBLINT_GEMFILE_VERSION" ]; then
-			ERBLINT_VERSION=$ERBLINT_GEMFILE_VERSION
+setup_erblint() {
+	echo '::group:: Installing erb-lint with extensions ... https://github.com/Shopify/erb-lint'
+	# if 'gemfile' erb-lint version selected
+	if [ "$INPUT_ERBLINT_VERSION" = "gemfile" ]; then
+		# if Gemfile.lock is here
+		if [ -f 'Gemfile.lock' ]; then
+			# grep for erb-lint version
+			ERBLINT_GEMFILE_VERSION=$(ruby -ne 'print $& if /^\s{4}erb_lint\s\(\K.*(?=\))/' Gemfile.lock)
+			# if erb-lint version found, then pass it to the gem install
+			# left it empty otherwise, so no version will be passed
+			if [ -n "$ERBLINT_GEMFILE_VERSION" ]; then
+				ERBLINT_VERSION=$ERBLINT_GEMFILE_VERSION
+			else
+				printf "Cannot get the erb-lint's version from Gemfile.lock. The latest version will be installed."
+			fi
 		else
-			printf "Cannot get the erb-lint's version from Gemfile.lock. The latest version will be installed."
+			printf 'Gemfile.lock not found. The latest version will be installed.'
 		fi
 	else
-		printf 'Gemfile.lock not found. The latest version will be installed.'
+		# set desired erb-lint version
+		ERBLINT_VERSION=$INPUT_ERBLINT_VERSION
 	fi
-else
-	# set desired erb-lint version
-	ERBLINT_VERSION=$INPUT_ERBLINT_VERSION
-fi
+	gem install -N erb_lint --version "${ERBLINT_VERSION}"
+	echo '::endgroup::'
+}
 
-gem install -N erb_lint --version "${ERBLINT_VERSION}"
-echo '::endgroup::'
+setup_bundler() {
+	if [ "${INPUT_USE_BUNDLER}" = "false" ]; then
+		BUNDLE_EXEC=""
+	else
+		BUNDLE_EXEC="bundle exec "
+	fi
+}
 
-if [ "${INPUT_USE_BUNDLER}" = "false" ]; then
-	BUNDLE_EXEC=""
-else
-	BUNDLE_EXEC="bundle exec "
-fi
+run_linter() {
+	echo '::group:: Running erb-lint with reviewdog 🐶 ...'
+	${BUNDLE_EXEC}erblint --lint-all --format compact |
+		reviewdog \
+			-efm="%f:%l:%c: %m" \
+			-reporter="${INPUT_REPORTER}" \
+			-filter-mode="${INPUT_FILTER_MODE}" \
+			-level="${INPUT_LEVEL}"
+	echo '::endgroup::'
+}
 
-echo '::group:: Running erb-lint with reviewdog 🐶 ...'
-${BUNDLE_EXEC}erblint --lint-all --format compact |
-	reviewdog \
-		-efm="%f:%l:%c: %m" \
-		-reporter="${INPUT_REPORTER}" \
-		-filter-mode="${INPUT_FILTER_MODE}" \
-		-level="${INPUT_LEVEL}"
-echo '::endgroup::'
+setup_git
+setup_reviewdog
+setup_erblint
+setup_bundler
+run_linter
